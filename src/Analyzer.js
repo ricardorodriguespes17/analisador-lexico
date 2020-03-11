7dcf04d6e289b6bffa91aa530d7a5fce0a4a23ff
Ricardo Rodrigues <39037180+ricardorodriguespes17@users.noreply.github.com>
2020-03-11 00:44:30 -0300

    Realizando detecção de inteiros e ponto fluante

--- src/Analyzer.js
+++ src/Analyzer.js
@@ -5,23 +5,86 @@ export default function analise(code) {
     if (code.trim() === '')
         return
 
-    //separa o texto em uma lista de palavras
-    var words = code
-        // .replace('\n', ' ')
-        // .split(' ')
-        // .filter(item => item !== '')
+    //remove as quebras de linha, separa o texto em uma lista de palavras
+    var words = code.replace(/\n/g, ' ')
+        .split(' ')
+        .filter(item => item !== '')
     console.log(words)
 
     var lexemes = []
 
     //mapeia o codigo e detecta os tokens
-    words.split('').map(letter => {
-        //mapeia letra por letra
-
+    words.map(word => {
+        var w = word.toLowerCase().split('')
+        var token = stateMachine(0, w, 0, null)
         
+        if(token !== null){
+            lexemes.push({ id: Math.random(), lexeme: w, token: token })
+        }
 
-        return letter
+        return word
     })
 
     return lexemes
+}
+
+function stateMachine(state, letters, position, result) {
+    //se a posicao a ser lida estiver fora do array, retorna o resultado
+    if (position >= letters.length)
+        return result
+    //letter recebe a letra da posicao lida
+    var letter = letters[position]
+
+    // console.log('Estado ' + state + "\nLetra: " + letter) 
+
+    //switch do estado atual
+    switch (state) {
+        case 0:
+            //estado 0 
+
+            if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].filter(num => num === letter).length === 1) {
+                return stateMachine(1, letters, position + 1, tokens.NUMERO_INTEIRO)
+            } else {
+                return stateMachine(-1, letters, position + 1, null)
+            }
+        case 1:
+            //estado 1
+
+            if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].filter(num => num === letter).length === 1) {
+                return stateMachine(1, letters, position + 1, tokens.NUMERO_INTEIRO)
+            } else if (letter === '.') {
+                return stateMachine(2, letters, position + 1, null)
+            } else {
+                return stateMachine(-1, letters, position + 1, null)
+            }
+        case 2:
+            //estado 2
+            
+            if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].filter(num => num === letter).length === 1) {
+                return stateMachine(2, letters, position + 1, tokens.NUMERO_REAL)
+            } else if (letter === 'e') {
+                return stateMachine(3, letters, position + 1, null)
+            } else {
+                return stateMachine(-1, letters, position + 1, null)
+            }
+        case 3: 
+            //estado 3
+            
+            if(letter === '-' || letter === '+'){
+                return stateMachine(4, letters, position + 1, null)
+            } else {
+                return stateMachine(-1, letters, position + 1, null)
+            }
+        case 4:
+            //estado 4
+
+            if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].filter(num => num === letter).length === 1) {
+                return stateMachine(2, letters, position + 1, tokens.NUMERO_REAL)
+            } else {
+                return stateMachine(-1, letters, position + 1, null)
+            }
+        default:
+            //estado morte
+            return null
+    }
 }
